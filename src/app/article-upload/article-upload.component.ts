92020968c726c620a75887557f4955b36ba10b58
eranda <erandasathsara@gmail.com>
2024-09-27 23:28:40 +0530

    issue fixed

--- src/app/article-upload/article-upload.component.ts
+++ src/app/article-upload/article-upload.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
 import { Message } from 'primeng/api';
-
+interface DataItem {
+  topic: string;
+  paragraph: string;
+}
 @Component({
   selector: 'app-article-upload',
   templateUrl: './article-upload.component.html',
   styleUrls: ['./article-upload.component.css'], // Corrected to styleUrls
 })
+
 export class ArticleUploadComponent {
   data: { topic: string, paragraph: string[] }[] = [];
   value: string | undefined;
   messages: Message[];
+  // Define the arrays with the correct type
+  relevantData: DataItem[] = [];
+  irrelevantData: DataItem[] = [];
+
+  // Method to handle the data uploaded event from the child
+  handleDataUpload(data: any) {
+    // Assuming the data structure contains clickbait_flag
+    console.log(data.data);
+    this.relevantData = data.data.filter((item: any) => item.clickbait_flag).map((item: any) => ({
+      topic: item.topic
+    }));
+    console.log("this.relevantData", this.relevantData);
+    this.irrelevantData = data.data.filter((item: any) => !item.clickbait_flag).map((item: any) => ({
+      topic: item.title
+    }));
+    console.log("this.irrelevantData", this.irrelevantData);
+  }
 
   constructor() {
     // Initializing messages inside the constructor
